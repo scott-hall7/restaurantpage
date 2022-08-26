8295eeffcce183d6406ab57d3aac09bca3f5b65a
scott-hall7 <scotthall179@gmail.com>
2022-08-26 06:35:35 -0500

    fixed menu formatting, cleaned up all code

--- src/menu.js
+++ src/menu.js
@@ -1,107 +1,110 @@
-function createMenu()   {
-    const menu = document.createElement("div");
-    menu.classList.add("menu");
+function createMenu() {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
 
-    const menuTitle = document.createElement("h3");
-    menuTitle.textContent = "Our Very Real Menu of Foods";
-    
-    menu.appendChild(menuTitle);
-    menu.appendChild(egg());
-    menu.appendChild(bacon());
-    menu.appendChild(waffle());
-    menu.appendChild(pineapple());
+  const menuTitle = document.createElement("h3");
+  menuTitle.textContent = "Our Very Real Menu of Foods";
 
-    return menu;
+  menu.appendChild(menuTitle);
+  menu.appendChild(egg());
+  menu.appendChild(bacon());
+  menu.appendChild(waffle());
+  menu.appendChild(pineapple());
+
+  return menu;
 }
 
 function egg() {
-    const egg = document.createElement("div");
-    egg.classList.add("menuItem");
+  const egg = document.createElement("div");
+  egg.classList.add("menuItem");
 
-    const eggTitle = document.createElement("h4");
-    eggTitle.textContent = "Hardboiled Egg";
+  const eggTitle = document.createElement("h4");
+  eggTitle.textContent = "Hardboiled Egg";
 
-    const eggDescription = document.createElement("p");
-    eggDescription.textContent = "Just a plain-old hardboiled egg. Emphasis on hard. Very real!";
+  const eggDescription = document.createElement("p");
+  eggDescription.textContent =
+    "Just a plain-old hardboiled egg. Emphasis on hard. Very real!";
 
-    const eggImage = document.createElement("img");
-    eggImage.src = "images/egg.jpeg";
-    eggImage.alt = "just an egg";
+  const eggImage = document.createElement("img");
+  eggImage.src = "images/egg.jpeg";
+  eggImage.alt = "just an egg";
 
-    egg.appendChild(eggTitle);
-    egg.appendChild(eggDescription);
-    egg.appendChild(eggImage);
+  egg.appendChild(eggTitle);
+  egg.appendChild(eggDescription);
+  egg.appendChild(eggImage);
 
-    return egg;
+  return egg;
 }
 
 function bacon() {
-    const bacon = document.createElement("div");
-    bacon.classList.add("menuItem");
+  const bacon = document.createElement("div");
+  bacon.classList.add("menuItem");
 
-    const baconTitle = document.createElement("h4");
-    baconTitle.textContent = "Bacon";
+  const baconTitle = document.createElement("h4");
+  baconTitle.textContent = "Bacon";
 
-    const baconDescription = document.createElement("p");
-    baconDescription.textContent = "We only cook our bacon extra crispy, so expect a crunch.";
+  const baconDescription = document.createElement("p");
+  baconDescription.textContent =
+    "We only cook our bacon extra crispy, so expect a crunch.";
 
-    const baconImage = document.createElement("img");
-    baconImage.src = "images/bacon.jpeg";
-    baconImage.alt = "just bacon";
+  const baconImage = document.createElement("img");
+  baconImage.src = "images/bacon.jpeg";
+  baconImage.alt = "just bacon";
 
-    bacon.appendChild(baconTitle);
-    bacon.appendChild(baconDescription);
-    bacon.appendChild(baconImage);
+  bacon.appendChild(baconTitle);
+  bacon.appendChild(baconDescription);
+  bacon.appendChild(baconImage);
 
-    return bacon;
+  return bacon;
 }
 
 function waffle() {
-    const waffle = document.createElement("div");
-    waffle.classList.add("menuItem");
+  const waffle = document.createElement("div");
+  waffle.classList.add("menuItem");
 
-    const waffleTitle = document.createElement("h4");
-    waffleTitle.textContent = "Waffle";
+  const waffleTitle = document.createElement("h4");
+  waffleTitle.textContent = "Waffle";
 
-    const waffleDescription = document.createElement("p");
-    waffleDescription.textContent = "Our waffles are also really hard, we struggle to cook soft things.";
+  const waffleDescription = document.createElement("p");
+  waffleDescription.textContent =
+    "Our waffles are also really hard, we struggle to cook soft things.";
 
-    const waffleImage = document.createElement("img");
-    waffleImage.src = "images/waffle.jpeg";
-    waffleImage.alt = "A waffle";
+  const waffleImage = document.createElement("img");
+  waffleImage.src = "images/waffle.jpeg";
+  waffleImage.alt = "A waffle";
 
-    waffle.appendChild(waffleTitle);
-    waffle.appendChild(waffleDescription);
-    waffle.appendChild(waffleImage);
+  waffle.appendChild(waffleTitle);
+  waffle.appendChild(waffleDescription);
+  waffle.appendChild(waffleImage);
 
-    return waffle;
+  return waffle;
 }
 
 function pineapple() {
-    const pineapple = document.createElement("div");
-    pineapple.classList.add("menuItem");
+  const pineapple = document.createElement("div");
+  pineapple.classList.add("menuItem");
 
-    const pineappleTitle = document.createElement("h4");
-    pineappleTitle.textContent = "Pineapple";
+  const pineappleTitle = document.createElement("h4");
+  pineappleTitle.textContent = "Pineapple";
 
-    const pineappleDescription = document.createElement("p");
-    pineappleDescription.textContent = "The softest thing on the menu.";
+  const pineappleDescription = document.createElement("p");
+  pineappleDescription.textContent = "The softest thing on the menu.";
 
-    const pineappleImage = document.createElement("img");
-    pineappleImage.src = "images/pineapple.jpeg";
-    pineappleImage.alt = "Pineapple";
+  const pineappleImage = document.createElement("img");
+  pineappleImage.src = "images/pineapple.jpeg";
+  pineappleImage.alt = "Pineapple";
 
-    pineapple.appendChild(pineappleTitle);
-    pineapple.appendChild(pineappleDescription);
-    pineapple.appendChild(pineappleImage);
+  pineapple.appendChild(pineappleTitle);
+  pineapple.appendChild(pineappleDescription);
+  pineapple.appendChild(pineappleImage);
 
-    return pineapple;
+  return pineapple;
 }
 
 function loadMenu() {
-    const main = document.getElementById("main");
-    main.textContent = "";
-    main.appendChild(createMenu());
+  const main = document.getElementById("main");
+  main.textContent = "";
+  main.appendChild(createMenu());
 }
 
 export default loadMenu;
--- src/website.js
+++ src/website.js
@@ -3,97 +3,92 @@ import loadMenu from "./menu";
 import loadContact from "./contact";
 
 function createHeader() {
-    const header = document.createElement("header");
-    
-    const mainTitle = document.createElement("p");
-    mainTitle.textContent = "Maybe Food?";
-    
-    
-    header.appendChild(mainTitle);
-    header.appendChild(createNav());
-    
-    return header;
+  const header = document.createElement("header");
+
+  const mainTitle = document.createElement("p");
+  mainTitle.textContent = "Maybe Food?";
+
+  header.appendChild(mainTitle);
+  header.appendChild(createNav());
+
+  return header;
 }
 
-function createNav()    {
-    const nav = document.createElement("nav")
+function createNav() {
+  const nav = document.createElement("nav");
 
-    const homeButton = document.createElement("button");
-    homeButton.classList.add("buttonNav");
-    homeButton.textContent = "Home";
-    homeButton.addEventListener("click", (event) => {
-        if(event.target.classList.contains("active")) return;
-        setActiveButton(homeButton);
-        loadHome();
-    })
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("buttonNav");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", (event) => {
+    if (event.target.classList.contains("active")) return;
+    setActiveButton(homeButton);
+    loadHome();
+  });
 
-    const menuButton = document.createElement("button");
-    menuButton.classList.add("buttonNav");
-    menuButton.textContent = "Menu";
-    menuButton.addEventListener("click", (event) => {
-        if(event.target.classList.contains("active")) return;
-        setActiveButton(menuButton);
-        loadMenu();
-    })
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("buttonNav");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", (event) => {
+    if (event.target.classList.contains("active")) return;
+    setActiveButton(menuButton);
+    loadMenu();
+  });
 
-    const contactButton = document.createElement("button");
-    contactButton.classList.add("buttonNav");
-    contactButton.textContent = "Contact";
-    contactButton.addEventListener("click", (event) => {
-        if(event.target.classList.contains("active")) return;
-        setActiveButton(contactButton);
-        loadContact();
-    })
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("buttonNav");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", (event) => {
+    if (event.target.classList.contains("active")) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
 
-    nav.appendChild(homeButton);
-    nav.appendChild(menuButton);
-    nav.appendChild(contactButton);
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
 
-    return nav
+  return nav;
 }
 
-function setActiveButton(button)    {
-    const buttons = document.querySelectorAll(".buttonNav");
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".buttonNav");
 
-    buttons.forEach((button) => {
-        if (button !== this)    {
-            button.classList.remove("active");
-        }
-    });
-    button.classList.add("active")
-};
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+  button.classList.add("active");
+}
 
-function createMain()   {
-    const main = document.createElement("main")
-    main.classList.add("main");
-    main.setAttribute("id", "main");
-    return main;
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
 }
 
 function createFooter() {
-    const footer = document.createElement("footer");
-    
-    const headChef = document.createElement("p");
-    headChef.textContent = "Head Chef: scott-hall7 on GitHub";
-    headChef.href = "https://github.com/scott-hall7"
+  const footer = document.createElement("footer");
 
-    footer.appendChild(headChef);
+  const headChef = document.createElement("p");
+  headChef.textContent = "Head Chef: scott-hall7 on GitHub";
+  headChef.href = "https://github.com/scott-hall7";
 
-    return footer;
+  footer.appendChild(headChef);
+
+  return footer;
 }
 
-function initalWebsite()  {
-    const content = document.getElementById("content");
-    content.appendChild(createHeader());
-    content.appendChild(createMain());
-    content.appendChild(createFooter());
+function initalWebsite() {
+  const content = document.getElementById("content");
+  content.appendChild(createHeader());
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
 
-    setActiveButton(document.querySelector(".buttonNav"));
-    loadHome();
+  setActiveButton(document.querySelector(".buttonNav"));
+  loadHome();
 }
 
 export default initalWebsite;
-
-
-
-
